e2739f47240c0ec01ef3433e625e61968e2f12eb
Aaron Larner <anlarner@gmail.com>
2016-03-04 13:17:13 -0600

    Update proxPort config and add production.js config override for heroku

---
+++ config/env/production.js
@@ -0,0 +1,6 @@
+module.exports = {
+	database: {
+		client: 'pg',
+		connection: process.env.PG_CONNECTION_STRING
+	}
+};
--- config/webserver.js
+++ config/webserver.js
@@ -1,10 +1,12 @@
 module.exports = {
 	hostname: 'localhost',
 	http: {
-		port: 3000
+		port: 3000,
+		proxyPort: 3000
 	}//,
 	// https: {
-	// 	port: 3434,
+	// 	port: 434,,
+	//	proxyPort: 3434
 	// 	keyPath: 'insecure.key',
 	// 	certPath: 'insecure.crt'
 	// }
