f2ff890951197066c7828047d44f4ce3912afe48
Aaron Larner <anlarner@gmail.com>
2016-03-04 21:04:28 -0600

    Update postinstall to output any errors

--- postinstall.js
+++ postinstall.js
@@ -9,4 +9,8 @@ let knex = require('knex')(config.database);
 knex.migrate.latest()
 .then(function() {
 	process.exit(0);
+})
+.catch(function(err) {
+	console.warn(err.toString());
+	process.exit(1);
 });
