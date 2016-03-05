23b92a7aec2591ed6cfb14e86e7ac77edf1b78ad
Aaron Larner <anlarner@gmail.com>
2016-03-04 20:54:30 -0600

    Add postinstall for heroku

---
+++ postinstall.js
@@ -0,0 +1,12 @@
+if(process.env.HEROKU_POSTINSTALL) {
+	process.env.NODE_ENV = 'heroku';
+}
+
+let config = require('./lib/config');
+let knex = require('knex')(config.database);
+
+
+knex.migrate.latest()
+.then(function() {
+	process.exit(0);
+});
