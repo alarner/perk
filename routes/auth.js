d5fd8915ed76e9c768151a96efe5f326a10fe8d4
Aaron Larner <anlarner@gmail.com>
2016-03-22 19:35:55 -0500

    Fix email redirects

--- routes/auth.js
+++ routes/auth.js
@@ -56,8 +56,20 @@ router.post('/email', validateAuthProfile, function(req, res, next) {
 		return res.redirect('/auth/email');
 	}
 	req.session._auth_profile.email = req.body.email;
-	authenticator(req, req.session._auth_access_token, req.session._auth_profile, req.session._auth_type, function(err, authModel) {
-		return res.redirect('/auth/finish');
+	authenticator(req, req.session._auth_access_token, req.session._auth_profile, req.session._auth_type, function(err, authModel, userModel) {
+		req.logIn(userModel, err => {
+			if(err) {
+				res.error.add('auth.UNKNOWN').send('/auth/login');
+			}
+			else {
+				if(req.accepts('html')) {
+					res.redirect(config.auth[authModel.get('type')].redirect || '/auth/finish');
+				}
+				else {
+					res.json(userModel.toJSON());
+				}
+			}
+		});
 	});
 
 });
