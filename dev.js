997f3d79d7f810e23002a3639d8452650881b2a3
Aaron Larner <anlarner@gmail.com>
2016-05-13 16:33:45 -0500

    Custom watchers

---
+++ dev.js
@@ -0,0 +1,8 @@
+let chokidar = require('chokidar');
+let grapher = require('sass-graph');
+
+// chokidar.watch('./public/styles/**/*.scss').on('all', sass);
+
+// function sass(event, path) {
+// 	console.log(path);
+// }
