1b97e7e9f29ccdab2503b1d0895358ee7fa26c78
Aaron Larner <anlarner@gmail.com>
2016-05-15 23:25:06 -0400

    Remove unnecessary files

--- dev.js
+++
@@ -1,8 +0,0 @@
-let chokidar = require('chokidar');
-let grapher = require('sass-graph');
-
-// chokidar.watch('./public/styles/**/*.scss').on('all', sass);
-
-// function sass(event, path) {
-// 	console.log(path);
-// }
