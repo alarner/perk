c1054ea0cc9f065a54293752cc018e4fb5fde987
travis <t3patterson@gmail.com>
2016-06-15 14:18:02 -0500

    oops, 6 spaces, not 4

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ let loggedIn = require('../lib/middleware/logged-in');
 let router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: "Perk Framework"
-    });
+      res.render('index', {
+            title: "Perk Framework"
+      });
 });
 
 router.get('/dashboard', loggedIn, function(req, res, next) {
