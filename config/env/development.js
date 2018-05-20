903f2b59042fa04e2e20bec4190e336515592875
Aaron Larner <aaron@larner.com>
2018-05-20 18:24:48 -0400

    Remove old directory structure

--- config/env/development.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	build: {
-		scripts: {
-			loader: 'webpack'
-		}
-	}
-};
--- models/User.js
+++
@@ -1,8 +0,0 @@
-require('./Authentication');
-module.exports = bookshelf.model('User', {
-	tableName: 'users',
-	hasTimestamps: ['createdAt', 'updatedAt', 'deletedAt'],
-	authentication: function() {
-		return this.hasMany('Authentication', 'userId');
-	}
-});
