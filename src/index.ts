6eb032a91108715ec87674a3df710187fb5cc1e2
Aaron Larner <alarner@users.noreply.github.com>
2020-10-16 07:22:39 -0400

    Remove requires from index.js

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-const HTTPError = require("./HTTPError");
-const HTTPRedirect = require("./HTTPRedirect");
-const server = require("./server");
-const model = require("./model");
-const db = require("./db");
-const configBuilder = require("./configBuilder");
-const bootstrap = require("./bootstrap");
-
 export * as HTTPError from "./HTTPError";
 export { HTTPRedirect } from "./HTTPRedirect";
 export { server } from "./server";
